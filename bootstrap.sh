b5c4ec2b6438a383792cbd9b464f4d5be21800ee
Tatsuro Yasukawa <t.yasukawa01@gmail.com>
2015-04-18 16:50:32 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
-echo -ne '\n' | git clone git://github.com/astrails/dotvim.git
+git clone git://github.com/astrails/dotvim.git
 ln -sfn dotvim .vim
 ln -sfn dotvim/vimrc .vimrc
-cd .vim; make install
+(cd .vim; echo -ne '\n' | make install)
 
 echo "install color schema"
 
