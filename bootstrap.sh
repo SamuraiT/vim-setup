3d149a574aec91e09e1184a80c1b0833fa78a9e8
Tatsuro Yasukawa <t.yasukawa01@gmail.com>
2015-04-18 16:40:42 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-git clone git://github.com/astrails/dotvim.git
+echo -ne '\n' | git clone git://github.com/astrails/dotvim.git
 ln -sfn dotvim .vim
 ln -sfn dotvim/vimrc .vimrc
 cd .vim; make install
@@ -24,4 +24,4 @@ syntax on
 let g:molokai_original = 1
 let g:rehash256 = 1
 set background=dark
-' >> ~/.vimrc.after
+' > ~/.vimrc.after
