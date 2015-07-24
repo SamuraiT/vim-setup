06509ea6b7486406ad70c0b04211cf0176c3bc3e
Tatsuro Yasukawa <t.yasukawa01@gmail.com>
2015-07-25 00:46:28 +0900

    Update install-color-scheme.sh

--- install-color-scheme.sh
+++ install-color-scheme.sh
@@ -14,10 +14,24 @@ set colorcolumn=+1
 set tabstop=2
 set shiftwidth=2
 set softtabstop=2
+set clipboard+=unnamed
+
 " add color schema
 colorscheme molokai
 syntax on
 let g:molokai_original = 1
 let g:rehash256 = 1
 set background=dark
+
+function! StripTrailingWhitespaces()
+let l = line(".")
+let c = col(".")
+%s/\s+$//e
+call cursor(l, c)
+endfunction
+augroup AutoStripTrailingWhitespaces
+autocmd!
+autocmd BufWritePre * :call StripTrailingWhitespaces()
+augroup END
 ' > ~/.vimrc.after
+
