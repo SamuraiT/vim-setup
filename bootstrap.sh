896a5103b3bf5f11f085689306083303350e9dc3
Tatsuro Yasukawa <t.yasukawa01@gmail.com>
2015-07-26 14:33:18 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -38,6 +38,21 @@ syntax on
 let g:molokai_original = 1
 let g:rehash256 = 1
 set background=dark
+
+
+set clipboard+=unnamed
+
+function! StripTrailingWhitespaces()
+  let l = line(".")
+  let c = col(".")
+  %s/\s\+$//e
+  call cursor(l, c)
+endfunction
+augroup AutoStripTrailingWhitespaces
+  autocmd!
+  autocmd BufWritePre * :call StripTrailingWhitespaces()
+augroup END
+
 ' > ~/.vimrc.after
 
 else
